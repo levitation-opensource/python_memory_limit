65857b9edb032015d39d6f24cc3554e66b2b2afb
Roland Pihlakas <roland@simplify.ee>
2020-10-20 07:05:29 +0300

    Update windows_jobobject.py

--- windows_jobobject.py
+++ windows_jobobject.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 #
-# Roland Pihlakas, 2017
-# roland@simplify.ee
+# Roland Pihlakas, 2017, roland@simplify.ee
+# Aya, 2013, https://stackoverflow.com/users/172176/aya
 #
 
 
